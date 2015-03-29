3c2fd23ae1c6ff6e1408f858d3f7e551abffff56
ukuhnhardt <ukuhnhardt@gmail.com>
2015-03-29 20:05:27 +1000

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+Simple HTTP Router
+====================
+
+A simple router for node.js [http](https://nodejs.org/api/http.html) module. Register routes express.js style and delegate HTTP requests directly to the router.
+
+## Installation
+
+
+## Usage
---
+++ test/router.js
@@ -0,0 +1,36 @@
+var expect = require('expect.js')
+    , httpMocks = require('node-mocks-http')
+    ;
+
+
+describe("Router", function () {
+    var router;
+    beforeEach(function (done) {
+        router = require('../lib/router');
+        done();
+    });
+    it('get /hello', function (done) {
+
+        var request = {url: '/hello', method: 'GET'};
+        var response = httpMocks.createResponse();
+//      var response = ServerResponse();
+
+        router.get('/hello', function (req, resp) {
+            expect(resp).to.be(response);
+            done();
+        });
+        router.route(request, response);
+    });
+    it('don\'t get /helloworld', function (done) {
+
+        var request = {url: '/helloworld', method: 'GET'};
+        var response = httpMocks.createResponse();
+
+        router.get('/hello', function (req, resp) {
+            return;
+        });
+        router.route(request, response);
+        expect(response.statusCode).to.be(404);
+        done();
+    });
+});
